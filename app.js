c905f0b205a94ff9c54445620e6894d4fb1b1c3c
ayyshaa <qacarovaayse@gmail.com>
2024-09-03 18:08:10 +0400

    myfunc

---
+++ app.js
@@ -0,0 +1,11 @@
+let rubl = prompt("rubl daxil edin:");
+let dollar = prompt("dollar daxil edin:");
+let rublnew = dollar * 75;
+
+function myFunc(rubl,rublnew){
+
+    console.log(+rubl + +rublnew);
+}
+myFunc(rubl,rublnew);
+
+
